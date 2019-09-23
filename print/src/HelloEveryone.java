a30501eec66b5de6d588fbfe0fff8db2fe5cee04
Martin <martin.horn@khk.ee>
2019-09-23 09:39:57 +0300

    Hello world! (And all the people of the world)

---
+++ print/src/HelloEveryone.java
@@ -0,0 +1,6 @@
+public class HelloEveryone {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n" +
+                "(And all the people of the world)");
+    }
+}
