78763809c04e06676c3c6f7b600a9523f0a10272
Martin <martin.horn@khk.ee>
2019-09-24 21:44:09 +0300

    Varying variables

--- print/src/Variables.java
+++ print/src/Variables.java
@@ -1,15 +1,15 @@
 public class Variables {
     public static void main(String[] args) {
         String text = "Chickens:";
-        int number1 = 3;
+        int number1 = 9000;
         System.out.println(text + "\n" + number1);
 
         String text2 = "Bacon (kg):";
-        double number2 = 5.5;
+        double number2 = 0.1;
         System.out.println(text2 + "\n" + number2);
 
         String text3 = "A tractor:";
-        String number3 = "There is none";
+        String number3 = "Zetor";
         System.out.println(text3 + "\n" + number3);
 
         System.out.println("\n" + "In a nutshell:");
