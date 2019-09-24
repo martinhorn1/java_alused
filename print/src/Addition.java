196d8400d51b761d646ab13e6c036eb90690fb62
Martin <martin.horn@khk.ee>
2019-09-24 21:51:25 +0300

    Addition

---
+++ print/src/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 2346;
+        int b = 53645;
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
