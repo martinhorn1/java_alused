35741cd9080293e96b1076d002bca4340e3af5f5
Martin <martin.horn@khk.ee>
2019-09-24 21:52:50 +0300

    Multiplication

---
+++ print/src/Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 277;
+        int b = 111;
+
+        System.out.println(a + " * " + b + " = " + (a * b));
+    }
+}
