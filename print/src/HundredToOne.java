eb5f33970be2664485021c144e3150dbd5581215
Martin <martin.horn@khk.ee>
2019-09-30 11:07:27 +0300

    Week 2, Exercise 28: From hundred to one

---
+++ print/src/HundredToOne.java
@@ -0,0 +1,10 @@
+public class HundredToOne {
+    public static void main(String[] args) {
+        int read = 100;
+
+        while (read >= 1) {
+            System.out.println(read);
+            read--;
+        }
+    }
+}
