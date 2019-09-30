0b6d6fb943ae1971277c8c2e451a065c31e10bf2
Martin <martin.horn@khk.ee>
2019-09-30 11:08:41 +0300

    Week 2, Exercise 29: Even numbers

---
+++ print/src/EvenNumbers.java
@@ -0,0 +1,10 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int read = 2;
+
+        while (read <= 100) {
+            System.out.println(read);
+            read += 2;
+        }
+    }
+}
