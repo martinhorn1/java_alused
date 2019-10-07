54a4bc71ea46df00d38dcefdc83491905a4bba07
Martin <martin.horn@khk.ee>
2019-10-07 13:39:28 +0300

    Week 3, Exercise 60: Words in alphabetical order

---
+++ print/src/WordsInAlphabetical.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class WordsInAlphabetical {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+        while (true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+            if (word.isEmpty()) {
+                break;
+            }
+            words.add(word);
+        }
+        System.out.println("You typed the following words:");
+        Collections.sort(words);
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
