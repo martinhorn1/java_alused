32a9b0848bf1f1005f68b9fa17a050910ffb6eec
Martin <martin.horn@khk.ee>
2019-10-08 14:54:12 +0300

    Week 3, Exercise 65: The lengths of the Strings

---
+++ print/src/TheLengthsOfStrings.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class TheLengthsOfStrings {
+    public static ArrayList<Integer> lengths(ArrayList<String> list) {
+        ArrayList<Integer> lengthList = new ArrayList<>();
+        for (String s : list) {
+            lengthList.add(s.length());
+        }
+        return lengthList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Hallo");
+        list.add("Moi");
+        list.add("Benvenuto!");
+        list.add("badger badger badger badger");
+        ArrayList<Integer> lengths = lengths(list);
+
+        System.out.println("The lengths of the strings: " + lengths);
+    }
+}
