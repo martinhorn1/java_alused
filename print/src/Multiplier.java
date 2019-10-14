737370194044aaeb5deeef74588c195ec1924538
Martin <martin.horn@khk.ee>
2019-10-14 14:02:43 +0300

    Week 4, Exercise 74: Multiplier

---
+++ print/src/Multiplier.java
@@ -0,0 +1,21 @@
+public class Multiplier {
+    private int number;
+
+    public Multiplier(int number) {
+        this.number = number;
+    }
+
+    public int multiply(int otherNumber) {
+        return otherNumber*number;
+    }
+
+    public static void main(String[] args) {
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultiplier.multiply(2): " + threeMultiplier.multiply(2));
+        Multiplier fourMultiplier = new Multiplier(4);
+        System.out.println("fourMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+
+        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
+        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
+    }
+}
