f86b8c6d1393967d87f141bdd45abfb82daa77d4
Martin <martin.horn@khk.ee>
2019-11-05 13:32:14 +0200

    Week 5, Exercise 92: Difference of two dates

--- print/src/MyDate.java
+++ print/src/MyDate.java
@@ -57,6 +57,23 @@ public class MyDate {
         return newMyDate;
     }
 
+//    Exercise 92
+    public int differenceInYears(MyDate comparedDate) {
+        if (this.year > comparedDate.year) {
+            if (this.month >= comparedDate.month && this.month >= comparedDate.month) {
+                return Math.abs(comparedDate.year - this.year);
+            } else {
+                return Math.abs(comparedDate.year - this.year) - 1;
+            }
+        }
+        else {
+            if (comparedDate.month >= this.month && comparedDate.month >= this.month) {
+                return Math.abs(this.year - comparedDate.year);
+            } else {
+                return Math.abs(this.year - comparedDate.year) - 1;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         MyDate day = new MyDate(25, 2, 2011);
@@ -67,6 +84,21 @@ public class MyDate {
         }
         System.out.println("This week's Friday is " + day);
         System.out.println("The date 790 days from this week's Friday is  " + day.afterNumberOfDays(790));
+
+        System.out.println("-----------");
+        System.out.println("Exercise 92");
+        System.out.println("-----------");
+
+        MyDate first = new MyDate(24, 12, 2009);
+        MyDate second = new MyDate(1, 1, 2011);
+        MyDate third = new MyDate(25, 12, 2010);
+
+        System.out.println( first + " and " + second + " difference in years: " + second.differenceInYears(first) );
+        System.out.println( second + " and " + first + " difference in years: " + first.differenceInYears(second) );
+        System.out.println( first + " and " + third + " difference in years: " + third.differenceInYears(first) );
+        System.out.println( third + " and " + first + " difference in years: " + first.differenceInYears(third) );
+        System.out.println( third + " and " + second + " difference in years: " + second.differenceInYears(third) );
+        System.out.println( second + " and " + third + " difference in years: " + third.differenceInYears(second) );
     }
 
 }
