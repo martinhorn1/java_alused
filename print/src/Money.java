c59903082d09763bb6431526068f6a958add8526
Martin <martin.horn@khk.ee>
2019-11-18 22:24:38 +0200

    Week 6, Exercise 95.2: Less

--- print/src/Money.java
+++ print/src/Money.java
@@ -42,23 +42,30 @@ public class Money {
         return new Money(newEuros, newCents);
     }
 
+    public boolean less(Money compared) {
+        if (euros < compared.euros) {
+            return true;
+        }
+        else if (euros == compared.euros) {
+            if (cents < compared.cents) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Money a = new Money(10,0);
-        Money b = new Money(5,0);
+        Money b = new Money(3,0);
+        Money c = new Money(5,0);
 
-        Money c = a.plus(b);
+        System.out.println(a.less(b));  // false
+        System.out.println(b.less(c));  // true
 
-        System.out.println(a);  // 10.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
-
-        a = a.plus(c);          // NOTE: new Money-object is created and reference to that
-        //           is assigned to variable a.
-        //       The Money object 10.00e that variable a used to hold
-        //           is not referenced anymore
-
-        System.out.println(a);  // 25.00e
-        System.out.println(b);  // 5.00e
-        System.out.println(c);  // 15.00e
     }
 }
