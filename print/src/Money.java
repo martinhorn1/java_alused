de91a037ff07b2893a2d4813879261d8db7a6be0
Martin <martin.horn@khk.ee>
2019-11-18 22:17:26 +0200

    Week 6, Exercise 95.1: Plus

---
+++ print/src/Money.java
@@ -0,0 +1,64 @@
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros += cents / 100;
+            cents %= 100;
+        }
+
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros(){
+        return euros;
+    }
+
+    public int cents(){
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+
+    public Money plus(Money added) {
+        int newEuros = euros + added.euros();
+        int newCents = cents + added.cents();
+
+        if (newCents > 99) {
+            newEuros ++;
+            newCents -= 100;
+        }
+        return new Money(newEuros, newCents);
+    }
+
+    public static void main(String[] args) {
+        Money a = new Money(10,0);
+        Money b = new Money(5,0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
+
+        a = a.plus(c);          // NOTE: new Money-object is created and reference to that
+        //           is assigned to variable a.
+        //       The Money object 10.00e that variable a used to hold
+        //           is not referenced anymore
+
+        System.out.println(a);  // 25.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
+    }
+}
