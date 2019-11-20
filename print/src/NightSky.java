590cd60c2e4960167d5dce82df137edf674e59cd
Martin <martin.horn@khk.ee>
2019-11-20 11:50:49 +0200

    Week 6, Exercise 100.3: Counting the number of stars

--- print/src/NightSky.java
+++ print/src/NightSky.java
@@ -4,30 +4,35 @@ public class NightSky {
     public double density;
     public int width;
     public int height;
+    public int starsInLastPrint;
 
     public NightSky(double density) {
         this.density = density;
-        width = 20;
-        height = 10;
+        this.width = 20;
+        this.height = 10;
+        this.starsInLastPrint = 0;
     }
 
     public NightSky(int width, int height) {
         this.width = width;
         this.height = height;
-        density = 0.1;
+        this.density = 0.1;
+        this.starsInLastPrint = 0;
     }
 
     public NightSky(double density, int width, int height) {
         this.density = density;
         this.width = width;
         this.height = height;
+        this.starsInLastPrint = 0;
     }
 
     public void printLine() {
         for (int i = 0; i < width; i++) {
-            boolean val = new Random().nextInt((100/(int)(density*100)))==0;
+            boolean val = new Random().nextDouble() <= this.density;
             if (val) {
                 System.out.print("*");
+                this.starsInLastPrint++;
             }
             else {
                 System.out.print(" ");
@@ -36,14 +41,24 @@ public class NightSky {
     }
 
     public void print() {
+        this.starsInLastPrint = 0;
         for (int i = 0; i < height; i++) {
-            printLine();
+            this.printLine();
             System.out.println("");
         }
+    }
+
+    public int starsInLastPrint() {
+        return this.starsInLastPrint;
     }
 
     public static void main(String[] args) {
         NightSky NightSky = new NightSky(8, 4);
         NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+        System.out.println("");
+
+        NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
     }
 }
