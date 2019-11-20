d78118348b8d6b9590f8d0181e13e4ea9c2f2057
Martin <martin.horn@khk.ee>
2019-11-20 11:06:22 +0200

    Week 6, Exercise 99: Array to stars

---
+++ print/src/ArrayToStars.java
@@ -0,0 +1,15 @@
+public class ArrayToStars {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayAsStars(array);
+    }
+
+    public static void printArrayAsStars(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
