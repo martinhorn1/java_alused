2ef2aa078817e9c0c26dafbac6898a58968a3e41
Martin <martin.horn@khk.ee>
2019-11-20 12:19:23 +0200

    Week 6, Exercise 101.1: Book

---
+++ print/src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    public String title;
+    public String publisher;
+    public int year;
+
+    public Book(String title, String publisher, int year) {
+        this.title = title;
+        this.publisher = publisher;
+        this.year = year;
+    }
+
+    public String title() {
+        return this.title;
+    }
+
+    public String publisher() {
+        return this.publisher;
+    }
+
+    public int year() {
+        return this.year;
+    }
+
+    public String toString() {
+        return this.title + ", " + this.publisher + ", " + this.year;
+    }
+
+    public static void main(String[] args) {
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        System.out.println(cheese.title());
+        System.out.println(cheese.publisher());
+        System.out.println(cheese.year());
+
+        System.out.println(cheese);
+    }
+}
