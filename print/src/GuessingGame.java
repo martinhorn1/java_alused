e0b6098e38fada38ec4811395d28b10514bbc11d
Martin <martin.horn@khk.ee>
2019-11-25 21:45:01 +0200

    Week 6, Exercise 105: Guessing game

--- print/src/GuessingGame.java
+++ print/src/GuessingGame.java
@@ -1,32 +1,61 @@
 import java.util.Scanner;
-import java.lang.Math;
-public class GuessingGame {
-    public static void main(String[] args) {
-        int game = drawNumber();
-        int counter = 0;
-        while (true) {
-            System.out.print("Guess the number: ");
-            Scanner read = new Scanner(System.in);
-            int guess = Integer.parseInt(read.nextLine());
 
-            if (guess == game) {
-                counter += 1;
-                System.out.println("Congratulations, your guess is correct, it took you " + counter + " tries");
-                break;
-            }
-            else if (guess < game){
-                counter += 1;
-                System.out.println("The number is greater, guesses made: " + counter);
-            }
-            else if (guess > game){
-                counter += 1;
-                System.out.println("The number is lesser, guesses made: " + counter);
-            }
-        }
+public class GuessingGame {
+
+    private Scanner reader;
+
+    public GuessingGame() {
+        this.reader = new Scanner(System.in);
     }
 
-    public static int drawNumber() {
-        int number = (int)(Math.random()*10);
-        return number;
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(lowerLimit, upperLimit);
+
+        // write the game logic here
+        while (lowerLimit != upperLimit) {
+            int average = average(lowerLimit, upperLimit);
+            if (isGreaterThan(average)) {
+                lowerLimit = average + 1;
+            }
+            else {
+                upperLimit = average;
+            }
+        }
+        System.out.println("--------");
+        System.out.println("The number you're thinking of is " + lowerLimit);
+        System.out.println("--------");
+    }
+
+    // implement here the methods isGreaterThan and average
+    public boolean isGreaterThan(int value) {
+        System.out.print("Is your number greater than " + value + "? (y/n) ");
+        return reader.nextLine().equals("y");
+    }
+
+    public int average(int first, int second) {
+        return (first + second) / 2;
+    }
+
+
+    public void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+
+        System.out.println("I promise you that I can guess the number you are thinking of with " + maxQuestions + " questions.");
+        System.out.println("Next I'll present you with a series of questions. Answer them honestly.");
+        System.out.println("---------------");
+    }
+
+    // a helper method:
+    public static int howManyTimesHalvable(int number) {
+        // we create a base two logarithm  of the given value
+        // Below we swap the base number to base two logarithms!
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        game.play(1,100);
     }
 }
