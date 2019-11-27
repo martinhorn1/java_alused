692278145fd591277d6eaedaa6177b553fb7fe96
Martin <martin.horn@khk.ee>
2019-11-27 11:53:23 +0200

    Week 7, Exercise 4.1: Class Thing

---
+++ print/src/Thing.java
@@ -0,0 +1,32 @@
+public class Thing {
+    public String name;
+    public int weight;
+
+    public Thing(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public String toString() {
+        return name + " (" + weight + " kg)";
+    }
+
+    public static void main(String[] args) {
+        Thing book = new Thing("Happiness in Three Steps", 2);
+        Thing mobile = new Thing("Nokia 3210", 1);
+
+        System.out.println("Book name: " + book.getName());
+        System.out.println("Book weight: " + book.getWeight());
+
+        System.out.println("Book: " + book);
+        System.out.println("Mobile: " + mobile);
+    }
+}
