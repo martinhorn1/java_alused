bd365d240783f47eb5c54d3dbdc6b121863cd620
Martin <martin.horn@khk.ee>
2019-11-27 12:03:22 +0200

    Week 7, Exercise 4.2: Class Suitcase

---
+++ print/src/Suitcase.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Suitcase {
+    public ArrayList<Thing> things;
+    public int maxWeight;
+    public int currentWeight = 0;
+
+    public Suitcase(int maxWeight) {
+        this.maxWeight = maxWeight;
+        things = new ArrayList<>();
+    }
+
+    public void addThing(Thing thing) {
+        if (thing.weight + currentWeight <= maxWeight) {
+            things.add(thing);
+            currentWeight += thing.weight;
+        }
+    }
+
+    public String toString() {
+        return things.size() + " things (" + currentWeight + " kg)";
+    }
+
+    public static void main(String[] args) {
+        Thing book = new Thing("Happiness in three steps", 2);
+        Thing mobile = new Thing("Nokia 3210", 1);
+        Thing brick = new Thing("Brick", 4);
+
+        Suitcase suitcase = new Suitcase(5);
+        System.out.println(suitcase);
+
+        suitcase.addThing(book);
+        System.out.println(suitcase);
+
+        suitcase.addThing(mobile);
+        System.out.println(suitcase);
+
+        suitcase.addThing(brick);
+        System.out.println(suitcase);
+    }
+}
