5a69766e5c49e7f00d92733a51e8e6b77a9503c0
Martin <martin.horn@khk.ee>
2019-11-27 12:33:06 +0200

    Week 7, Exercise 4.6: Container

---
+++ print/src/Container.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Container {
+    public ArrayList<Suitcase> suitcases;
+    public int maxWeight;
+
+    public Container(int maxWeight) {
+        this.suitcases = new ArrayList<>();
+        this.maxWeight = maxWeight;
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        if (totalWeight() + suitcase.totalWeight() <= maxWeight) {
+            suitcases.add(suitcase);
+        }
+    }
+
+    public int totalWeight() {
+        int total = 0;
+        for (Suitcase s : suitcases) {
+            total += s.totalWeight();
+        }
+        return total;
+    }
+
+    public String toString() {
+        return suitcases.size() + " suitcases (" + totalWeight() + " kg)";
+    }
+
+    public static void main(String[] args) {
+        Thing book = new Thing("Happiness in Three Steps", 2);
+        Thing mobile = new Thing("Nokia 3210", 1);
+        Thing brick = new Thing("Brick", 4);
+
+        Suitcase tomsCase = new Suitcase(10);
+        tomsCase.addThing(book);
+        tomsCase.addThing(mobile);
+
+        Suitcase georgesCase = new Suitcase(10);
+        georgesCase.addThing(brick);
+
+        Container container = new Container(1000);
+        container.addSuitcase(tomsCase);
+        container.addSuitcase(georgesCase);
+
+        System.out.println(container);
+    }
+}
