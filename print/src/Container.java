bb081d528e95a64fe24576a7bf2c390520037f66
Martin <martin.horn@khk.ee>
2019-11-27 12:50:34 +0200

    Week 7, Exercise 4.8: A lot of bricks

--- print/src/Container.java
+++ print/src/Container.java
@@ -33,23 +33,19 @@ public class Container {
         return suitcases.size() + " suitcases (" + totalWeight() + " kg)";
     }
 
-    public static void main(String[] args) {
-        Thing book = new Thing("Happiness in Three Steps", 2);
-        Thing mobile = new Thing("Nokia 3210", 1);
-        Thing brick = new Thing("Brick", 4);
-
-        Suitcase tomsCase = new Suitcase(10);
-        tomsCase.addThing(book);
-        tomsCase.addThing(mobile);
-
-        Suitcase georgesCase = new Suitcase(10);
-        georgesCase.addThing(brick);
-
+    public static void main(String[] Container) {
         Container container = new Container(1000);
-        container.addSuitcase(tomsCase);
-        container.addSuitcase(georgesCase);
+        addSuitcasesFullOfBricks(container);
+        System.out.println(container);
+    }
 
-        System.out.println("There are the following things in the container suitcases:");
-        container.printThings();
+    public static void addSuitcasesFullOfBricks(Container container) {
+        // adding 100 suitcases with one brick in each
+        for (int i = 0; i < 100; i++) {
+            Thing brick = new Thing("Brick", 22);
+            Suitcase a = new Suitcase(100);
+            a.addThing(brick);
+            container.addSuitcase(a);
+        }
     }
 }
