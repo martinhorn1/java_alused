1336c19e6d85a9cad0d06cf52bb31e718fa8528a
Martin <martin.horn@khk.ee>
2019-11-29 13:38:13 +0200

    Week 7, Exercise 7.5: Adding and Translating Words

--- print/src/TextUserInterface.java
+++ print/src/TextUserInterface.java
@@ -9,15 +9,39 @@ public class TextUserInterface {
         this.dictionary = dictionary;
     }
 
+    public void add(String word, String translation) {
+        dictionary.add(word, translation);
+    }
+
+    public String translate(String word) {
+        return dictionary.translate(word);
+    }
+
     public void start() {
-        System.out.println("Statement: ");
+        System.out.println("Statements: ");
+        System.out.println("\tadd - adds a word pair to the dictionary");
+        System.out.println("\ttranslate - asks a word and prints its translation");
         System.out.println("\tquit - quit the text user interface");
 
         while (true) {
+            System.out.print("\nStatement: ");
             String input = reader.nextLine();
             if (input.equals("quit")) {
                 System.out.println("Cheers!");
                 break;
+            }
+            else if (input.equals("add")) {
+                System.out.print("In Finnish: ");
+                String word = reader.nextLine();
+                System.out.print("Translation: ");
+                String translation = reader.nextLine();
+
+                add(word, translation);
+            }
+            else if (input.equals("translate")) {
+                System.out.print("Give a word: ");
+                String word = reader.nextLine();
+                System.out.println("Translation: " + translate(word));
             }
             else {
                 System.out.println("Unknown statement");
