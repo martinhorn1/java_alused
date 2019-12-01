5fb3122186f098f5865efa5dbfde9c9b1af98c9a
Martin <martin.horn@khk.ee>
2019-12-01 12:38:48 +0200

    Week 8, Exercise 9: Car Registration Centre

---
+++ print/src/RegistrationPlate.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class RegistrationPlate {
+    // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String regCode, String country) {
+        this.regCode = regCode;
+        this.country = country;
+    }
+
+    public String toString(){
+        return regCode+ " "+country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegistrationPlate that = (RegistrationPlate) o;
+        return regCode.equals(that.regCode) &&
+                country.equals(that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(regCode, country);
+    }
+
+    public static void main(String[] args) {
+        RegistrationPlate reg1 = new RegistrationPlate("FI", "ABC-123");
+        RegistrationPlate reg2 = new RegistrationPlate("FI", "UXE-465");
+        RegistrationPlate reg3 = new RegistrationPlate("D", "B WQ-431");
+
+        ArrayList<RegistrationPlate> finnish = new ArrayList<RegistrationPlate>();
+        finnish.add(reg1);
+        finnish.add(reg2);
+
+        RegistrationPlate first = new RegistrationPlate("FI", "ABC-123");
+        if (!finnish.contains(first)) {
+            finnish.add(first);
+        }
+        System.out.println("Finnish: " + finnish);
+        // if the equals method hasn't been overwritten, the same registration plate is repeated in the list
+
+        HashMap<RegistrationPlate, String> owners = new HashMap<RegistrationPlate, String>();
+        owners.put(reg1, "Arto");
+        owners.put(reg3, "Jürgen");
+
+        System.out.println("owners:");
+        System.out.println(owners.get(new RegistrationPlate("FI", "ABC-123")));
+        System.out.println(owners.get(new RegistrationPlate("D", "B WQ-431")));
+        // if the hashCode hasn't been overwritten, the owners are not found
+    }
+}
