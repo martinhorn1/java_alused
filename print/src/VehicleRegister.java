2443d89131ea19d80f504f43e5819a8c54d4c837
Martin <martin.horn@khk.ee>
2019-12-01 12:57:47 +0200

    Week 8, Exercise 9.2: The Owner, Based of the Registration Plate

---
+++ print/src/VehicleRegister.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class VehicleRegister {
+    public HashMap<RegistrationPlate, String> owners;
+
+    public VehicleRegister() {
+        owners = new HashMap<>();
+    }
+
+    public boolean add(RegistrationPlate plate, String owner) {
+        if (owners.containsKey(plate)) {
+            return false;
+        }
+        owners.put(plate, owner);
+        return true;
+    }
+
+    public String get(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return null;
+        }
+        return owners.get(plate);
+    }
+
+    public boolean delete(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return false;
+        }
+        owners.remove(plate);
+        return true;
+    }
+}
