eeb0954f268af0d3e7befa9a0861cad27b150578
Martin <martin.horn@khk.ee>
2019-12-26 16:10:24 +0200

    Week 8, Exercise 12.5: Shopping Basket

---
+++ print/src/ShoppingBasket.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingBasket {
+    public Map<String, Purchase> purchases;
+
+    public ShoppingBasket() {
+        purchases = new HashMap<>();
+    }
+
+    public void add(String product, int price) {
+        if (purchases.containsKey(product)) {
+            purchases.get(product).increaseAmount();
+        }
+        else {
+            purchases.put(product, new Purchase(product, 1, price));
+        }
+    }
+
+    public int price() {
+        int total = 0;
+        for (Purchase s : purchases.values()) {
+            total += s.price();
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        ShoppingBasket basket = new ShoppingBasket();
+        basket.add("milk", 3);
+        basket.add("buttermilk", 2);
+        basket.add("cheese", 5);
+        System.out.println("basket price: " + basket.price());
+        basket.add("computer", 899);
+        System.out.println("basket price: " + basket.price());
+    }
+}
