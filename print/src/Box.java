c5e0131eb1dba8d309ae5f35d5a418fbf59aad17
Martin <martin.horn@khk.ee>
2019-12-26 14:24:07 +0200

    Week 8, Exercise 11.4: Boxes are Stored too!

--- print/src/Box.java
+++ print/src/Box.java
@@ -32,15 +32,18 @@ public class Box implements ToBeStored {
 
     public static void main(String[] args) {
         Box box = new Box(10);
+        Box box2 = new Box(5);
+        Box box3 = new Box(9);
 
-        box.add( new Book1("Fedor Dostojevski", "Crime and Punishment", 2) ) ;
-        box.add( new Book1("Robert Martin", "Clean Code", 1) );
-        box.add( new Book1("Kent Beck", "Test Driven Development", 0.7) );
+        box2.add( new Book1("Fedor Dostojevski", "Crime and Punishment", 2) ) ;
+        box2.add( new Book1("Robert Martin", "Clean Code", 1) );
+        box2.add( new Book1("Kent Beck", "Test Driven Development", 0.7) );
 
-        box.add( new CD("Pink Floyd", "Dark Side of the Moon", 1973) );
-        box.add( new CD("Wigwam", "Nuclear Nightclub", 1975) );
-        box.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
+        box.add(box2);
+        box3.add(box2);
 
         System.out.println( box );
+        System.out.println( box2 );
+        System.out.println( box3 );
     }
 }
